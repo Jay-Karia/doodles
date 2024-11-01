c61279699af60a104a2c4d50ec17670771b198f0
Jay <jay.sanjay.karia@gmail.com>
2024-11-01 14:38:25 +0000

    chore: button story add

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,7 @@
-export default function Button() {
-  return <button className="text-blue-500">Click me</button>;
+import { ComponentProps } from "react";
+
+type ButtonProps = ComponentProps<"button">;
+
+export default function Button({...props} : ButtonProps) {
+  return <button className="text-blue-500" {...props}/>;
 }
