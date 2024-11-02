3500192c8f81c93d05534e750ef03fa86a74e62e
Jay <jay.sanjay.karia@gmail.com>
2024-11-02 13:06:56 +0530

    chore: added button styles
    using tailwind and other utilities

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -7,7 +7,7 @@ const meta: Meta = {
   parameters: {
     layout: "centered",
   },
-  tags: ['components', 'button'],
+  tags: ['components', 'button', "autodocs"],
 }
 
 export default meta
@@ -17,5 +17,26 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     children: "Click me",
+  }
+}
+
+export const Solid: Story = {
+  args: {
+    children: "Solid",
+    variant: "solid",
+  }
+}
+
+export const Ghost: Story = {
+  args: {
+    children: "Ghost",
+    variant: "ghost",
+  }
+}
+
+export const Outline: Story = {
+  args: {
+    children: "Outline",
+    variant: "outline",
   }
 }
