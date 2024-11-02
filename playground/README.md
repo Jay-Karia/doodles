6e1dbaeccc0514ebfe782a25a417dba5605ff691
Jay <jay.sanjay.karia@gmail.com>
2024-11-02 20:36:52 +0530

    feat: add playground

---
+++ playground/README.md
@@ -0,0 +1,3 @@
+# Playground
+
+Playing with the doodles component library
---
+++ playground/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+
+  return (
+    <div className="border-b p-4">
+    </div>
+  )
+}
+
+export default App
